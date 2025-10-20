ef154a8ff41a2d34d3d668974d21ea3ef63494a2
Jhonata Ezequiel <jhonataezequiel@hotmail.com>
2025-10-20 12:20:51 -0300

    asd

--- functions.sh
+++ functions.sh
@@ -206,8 +206,8 @@ aur_setup(){
         echo "Installing chaotic-aur"
         sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
         sudo pacman-key --lsign-key 3056513887B78AEB
-        sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'  # Add --noconfirm
-        sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  # Add --noconfirm
+        sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 
+        sudo pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
         sudo cp pacman.conf /etc/pacman.conf
         sudo pacman -Syu --noconfirm
     fi
